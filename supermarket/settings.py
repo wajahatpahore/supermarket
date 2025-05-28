64644f8d28fbd0ee874f41dbb4aca9dca652d543
wajahatpahore <wajahatpahore6@gmail.com>
2025-05-28 22:50:48 +0500

    Revert "Update base.html to load static files and set DEBUG to False in settings.py"
    This reverts commit 91d649efba0a5c9e38fcc8286739896b706e9c5f.

--- supermarket/settings.py
+++ supermarket/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6%q@w1ys#%v_)eu7408hpj1b-y@ljh5fj(lq-k3ya9)cpk&hn('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['wajahatpahore.pythonanywhere.com', 'localhost', '127.0.0.1', 'www.wajahatpahore.pythonanywhere.com']
 
